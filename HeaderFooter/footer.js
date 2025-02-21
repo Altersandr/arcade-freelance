985b1287295a83eca2f54d47dbc837db2a63ab8d
Marco Pelissero <pelisseromarco58@gmail.com>
2025-02-21 09:29:15 +0100

    nuovobranch

---
+++ HeaderFooter/footer.js
@@ -0,0 +1,65 @@
+document.write(`
+    <footer class="text-light pt-4">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3">
+                    <h5>Per i Freelance</h5>
+                    <ul class="list-unstyled">
+                        <li><a href="#" class="text-light">Trova Lavoro</a></li>
+                        <li><a href="#" class="text-light">Registrati</a></li>
+                        <li><a href="#" class="text-light">Consigli utili</a></li>
+                    </ul>
+                </div>
+                <div class="col-md-3">
+                    <h5>Per i Clienti</h5>
+                    <ul class="list-unstyled">
+                        <li><a href="#" class="text-light">Assumi un Professionista</a></li>
+                        <li><a href="#" class="text-light">Come Funziona</a></li>
+                        <li><a href="#" class="text-light">Prezzi e Tariffe</a></li>
+                        <li><a href="#" class="text-light">Recensioni clienti</a></li>
+                    </ul>
+                </div>
+                <div class="col-md-3">
+                    <h5>Categorie</h5>
+                    <ul class="list-unstyled">
+                        <li><a href="#" class="text-light">Sviluppo Web</a></li>
+                        <li><a href="#" class="text-light">Grafica e Design</a></li>
+                        <li><a href="#" class="text-light">Marketing</a></li>
+                         <li><a href="#" class="text-light">Scrittura e traduzione</a></li>
+                    </ul>
+                </div>
+                <div class="col-md-3">
+                    <h5>Assistenza</h5>
+                    <ul class="list-unstyled">
+                        <li><a href="#" class="text-light">Centro Assistenza</a></li>
+                        <li><a href="#" class="text-light">Contattaci</a></li>
+                        <li><a href="#" class="text-light">FAQ</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="d-flex justify-content-between align-items-center py-3 border-top mt-3">
+                <img src="../img/logo project work.png" alt="Logo Azienda" style="width: 100px;">
+                <p class="mb-0">&copy; 2025 Tutti i diritti riservati. Capitale Sociale: €1.000.000</p>
+                
+                <div class="social-icons">
+                    <img
+                      src="../img/x.png"
+                      alt="x"
+                      width="36px"
+                      height="36px"
+                    /><img
+                      src="../img/fb.webp"
+                      alt="fb"
+                      width="36px"
+                      height="36px"
+                    /><img
+                      src="../img/yt.webp"
+                      alt="yt"
+                      width="36px"
+                      height="36px"
+                    />
+                  </div>
+            </div>
+        </div>
+    </footer>
+    `);
